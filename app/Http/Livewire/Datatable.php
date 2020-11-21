1e4855fcb4b5c6fef936dc5b555c99a91451c03a
LittleBigProgramming <20643587+LittleBigProgramming@users.noreply.github.com>
2020-11-21 19:43:04 +0000

    Added Columns to show within the datatable using the getColumnListing through the Schema Facade. Since this is currently using the User table there are obviously columns which should not be shown, so these are passed into the reject method upon collecting the columns.

--- app/Http/Livewire/Datatable.php
+++ app/Http/Livewire/Datatable.php
@@ -2,27 +2,60 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Schema;
 use Livewire\Component;
 
 class Datatable extends Component
 {
     public $model;
+    public $columns;
+    protected $exclude;
 
+    /**
+     * @return mixed
+     */
     public function builder()
     {
         return $this->model::query();
     }
 
-    public function mount($model)
+    /**
+     * @return mixed
+     */
+    public function columns()
     {
-        $this->model = $model;
+        return collect(Schema::getColumnListing($this->builder()->getQuery()->from))
+            ->reject(function ($column) {
+                return in_array($column, $this->exclude);
+            })->toArray();
     }
 
+    /**
+     * @param $model
+     * @param string $exclude
+     */
+    public function mount($model, string $exclude = '')
+    {
+        $this->model = $model;
+        $this->exclude = explode(',', $exclude);
+        $this->columns = $this->columns();
+    }
+
+    /**
+     * @return mixed
+     */
     public function records()
     {
         return $this->builder()->get();
     }
 
+    /**
+     * @return
+     * \Illuminate\Contracts\Foundation\Application|
+     * \Illuminate\Contracts\View\Factory|
+     * \Illuminate\Contracts\View\View|
+     * \Illuminate\View\View
+     */
     public function render()
     {
         return view('livewire.datatable');
--- resources/views/livewire/datatable.blade.php
+++ resources/views/livewire/datatable.blade.php
@@ -1,14 +1,22 @@
-<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-    <table class="min-w-full divide-y divide-gray-200">
+<div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+    <table class="table-fixed min-w-full divide-y divide-gray-200">
         <thead>
-
+            <tr>
+                @foreach ($columns as $column)
+                    <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        {{ $column }}
+                    </th>
+                @endforeach
+            </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
             @foreach($this->records() as $record)
                 <tr>
-                    <td class="px-6 py-4 whitespace-nowrap">
-                        {{ $record->id }}
-                    </td>
+                    @foreach ($columns as $column)
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            {{ $record->{$column} }}
+                        </td>
+                    @endforeach
                 </tr>
             @endforeach
         </tbody>
